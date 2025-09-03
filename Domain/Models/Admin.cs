723e7c4994e8b6ba558f3f11c434ba8f917c7c20
GoodStuff15 <gustav.emil.eriksson@gmail.com>
2025-09-03 10:51:40 +0200

    Created baseline for domain entities (models)

---
+++ Domain/Models/Admin.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Models
+{
+    public class Admin
+    {
+        public int Id { get; set; }
+        public string UserName { get; set; } = string.Empty;
+
+        public string Email { get; set; } = string.Empty;
+
+        // Navigation
+        
+        public Borrower? Borrower { get; set; } 
+    }
+}
---
+++ Domain/Models/User.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string UserName { get; set; } = string.Empty;
+        public string Email { get; set; } = string.Empty;
+
+        public string Password { get; set; } = string.Empty;
+
+        // Is this user authorized to borrow?
+        public bool isAuthorized { get; set; }  
+
+        // Navigation
+
+        public Borrower? Borrower { get; set; }
+        
+    }
+}
