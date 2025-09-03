13a9f4f2cdd2b4ccfd752786d3d302cb355f0c2e
GoodStuff15 <gustav.emil.eriksson@gmail.com>
2025-09-03 12:20:27 +0200

    Created baseline for a generic repository + interface

---
+++ DataAccess/Repositories/GenericRepository.cs
@@ -0,0 +1,40 @@
+﻿using Infrastructure.Repositories.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Repositories
+{
+    public class GenericRepository<TEntity> where TEntity : class, IGenericRepository<TEntity>
+    {
+        public Task AddAsync(TEntity entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task Delete(TEntity entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<TEntity>> GetAllAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<TEntity?> GetByIdAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task Update(TEntity entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
+
+// https://learn.microsoft.com/en-us/aspnet/mvc/overview/older-versions/getting-started-with-ef-5-using-mvc-4/implementing-the-repository-and-unit-of-work-patterns-in-an-asp-net-mvc-application
+
