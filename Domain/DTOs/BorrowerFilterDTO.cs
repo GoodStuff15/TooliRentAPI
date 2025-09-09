f32ebe38bb3cbe2ddf58a2897a5e846b892b4b4a
GoodStuff15 <gustav.emil.eriksson@gmail.com>
2025-09-09 09:56:37 +0200

    Created baseline for borrower line

---
+++ Domain/DTOs/BorrowerFilterDTO.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.DTOs
+{
+    public record BorrowerFilterDTO
+    {
+        public string? Name { get; init; }
+        public string? Email { get; init; }
+        public string? PhoneNumber { get; init; }
+
+        public bool? IsSuspended { get; init; }
+
+        public DateOnly CreatedAfter { get; init; }
+        public DateOnly CreatedBefore { get; init; }
+
+        public bool hasBookings { get; init; }
+        public bool hasLateBookings { get; init; }  
+
+    }
+}
