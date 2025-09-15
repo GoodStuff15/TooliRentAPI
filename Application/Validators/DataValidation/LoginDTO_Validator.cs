f453bd384fe0333d49ec8754862a00efa6b25fb7
GoodStuff15 <gustav.emil.eriksson@gmail.com>
2025-09-15 08:46:42 +0200

    added logindto validator (basic)

---
+++ Application/Validators/DataValidation/LoginDTO_Validator.cs
@@ -0,0 +1,21 @@
+﻿using Domain.DTOs.IdentityDTOs;
+using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Validators.DataValidation
+{
+    public class LoginDTO_Validator : AbstractValidator<LoginDTO>
+    {
+        public LoginDTO_Validator() 
+        {
+            RuleFor(x =>  x.Username).NotEmpty();
+
+            RuleFor(x => x.Password).NotEmpty();
+        
+        }
+    }
+}
